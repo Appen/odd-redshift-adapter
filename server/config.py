3d810a645f71b32f3d6318a45cd14397b2d705ef
RBlack463 <Sniper@666>
2021-08-01 14:43:51 +0300

    docker-compose

--- server/config.py
+++ server/config.py
@@ -18,13 +18,13 @@ def get_env(env: str, default_value: Any = None) -> str:
 
 
 class BaseConfig:
-    ODD_HOST = get_env('PGHOST', get_env('ODD_DATA_SOURCE_NAME', 'localhost'))
-    ODD_PORT = get_env('PGPORT', '5439')
-    ODD_DATABASE = get_env('PGDATABASE', '')
-    ODD_USER = get_env('PGUSER', '')
-    ODD_PASSWORD = get_env('PGPASSWORD', '')
+    ODD_HOST = get_env('REDSHIFT_HOST', get_env('ODD_DATA_SOURCE_NAME', 'localhost'))
+    ODD_PORT = get_env('REDSHIFT_PORT', '5439')
+    ODD_DATABASE = get_env('REDSHIFT_DATABASE', '')
+    ODD_USER = get_env('REDSHIFT_USER', '')
+    ODD_PASSWORD = get_env('REDSHIFT_PASSWORD', '')
 
-    ODD_DATA_SOURCE_NAME = get_env('ODD_DATA_SOURCE_NAME', get_env('PGHOST', 'localhost'))
+    ODD_DATA_SOURCE_NAME = get_env('ODD_DATA_SOURCE_NAME', get_env('REDSHIFT_HOST', 'localhost'))
     ODD_DATA_SOURCE = get_env('ODD_DATA_SOURCE', 'postgresql://')
 
     SCHEDULER_INTERVAL_MINUTES = get_env('SCHEDULER_INTERVAL_MINUTES', 60)
@@ -42,10 +42,10 @@ def log_env_vars(config: dict):
     logging.info('Environment variables:')
     logging.info(f'ODD_DATA_SOURCE_NAME={config["ODD_DATA_SOURCE_NAME"]}')
     logging.info(f'ODD_DATA_SOURCE={config["ODD_DATA_SOURCE"]}')
-    logging.info(f'PGHOST={config["ODD_HOST"]}')
-    logging.info(f'PGPORT={config["ODD_PORT"]}')
-    logging.info(f'PGDATABASE={config["ODD_DATABASE"]}')
-    logging.info(f'PGUSER={config["ODD_USER"]}')
+    logging.info(f'REDSHIFT_HOST={config["ODD_HOST"]}')
+    logging.info(f'REDSHIFT_PORT={config["ODD_PORT"]}')
+    logging.info(f'REDSHIFT_DATABASE={config["ODD_DATABASE"]}')
+    logging.info(f'REDSHIFT_USER={config["ODD_USER"]}')
     if config["ODD_PASSWORD"] != '':
-        logging.info('PGPASSWORD=***')
+        logging.info('REDSHIFT_PASSWORD=***')
     logging.info(f'SCHEDULER_INTERVAL_MINUTES={config["SCHEDULER_INTERVAL_MINUTES"]}')
